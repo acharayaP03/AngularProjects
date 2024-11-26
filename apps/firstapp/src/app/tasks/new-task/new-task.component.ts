e313cfb8ccaa6d74ba7480c34b9452ef191a51e0
acharayaP03 <acharyaP03@gmail.com>
2024-11-26 17:25:35 +1100

    create add new task component

---
+++ apps/firstapp/src/app/tasks/new-task/new-task.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-new-task',
+  imports: [],
+  templateUrl: './new-task.component.html',
+  styleUrl: './new-task.component.scss',
+})
+export class NewTaskComponent {
+  @Output() closeDialog = new EventEmitter<void>();
+
+  onCloseDialog() {
+    this.closeDialog.emit();
+  }
+}
--- apps/firstapp/src/app/tasks/tasks.component.ts
+++ apps/firstapp/src/app/tasks/tasks.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from './task/task.component';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { tasks as assignedUserTasks } from '../Data/appData';
 
 @Component({
   selector: 'app-tasks',
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.scss',
   standalone: true,
@@ -12,39 +14,23 @@ export class TasksComponent {
   @Input({ required: true }) userId!: string;
   @Input({ required: true }) name: string | undefined;
 
-  tasks = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ];
+  assignNewTask = false;
+
+  tasks = assignedUserTasks;
 
   get selectedUserTasks() {
     return this.tasks.filter((t) => t.userId === this.userId);
   }
 
   inCompleteTask(id: string) {
-    console.log('emitted event: ', id);
-
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onAssignNewTask() {
+    this.assignNewTask = true;
+  }
+
+  closeAssignNewTaskDialog() {
+    this.assignNewTask = false;
   }
 }
