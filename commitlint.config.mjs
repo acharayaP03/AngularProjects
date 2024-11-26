76890cdb1b5eab2ef865116db0779e036324651a
acharayaP03 <acharyaP03@gmail.com>
2024-11-26 11:53:59 +1100

    loosen restriction for commit

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,1 +1,7 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-empty': [0], // Allow empty subjects
+    'type-empty': [0],
+  },
+};
