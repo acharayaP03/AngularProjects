dd7949d407b45cc32269f98b740dd9117a1181d8
acharayaP03 <acharyaP03@gmail.com>
2024-11-26 15:24:41 +1100

    app component

--- apps/firstapp/src/app/app.component.ts
+++ apps/firstapp/src/app/app.component.ts
@@ -1,26 +1,26 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { TaskComponent } from './task/task.component';
+import { TasksComponent } from './tasks/tasks.component';
 import { USERS } from './users';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserComponent, TaskComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'firstapp';
   userName: string = '';
-  selectedId?: string;
+  selectedUserId?: string;
   users = USERS;
 
   get selectedUser() {
-    return this.users.find((user) => user.id === this.selectedId);
+    return this.users.find((user) => user.id === this.selectedUserId);
   }
 
   onSelectedUser(id: string) {
-    this.selectedId = id;
+    this.selectedUserId = id;
   }
 }
