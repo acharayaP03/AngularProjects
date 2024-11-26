78add53930f477a9b98643c31a5cc8a68a65cac3
acharayaP03 <acharyaP03@gmail.com>
2024-11-26 21:05:55 +1100

    complete service

--- apps/firstapp/src/app/tasks/tasks.service.ts
+++ apps/firstapp/src/app/tasks/tasks.service.ts
@@ -8,6 +8,14 @@ Injectable({
 export class TaskService {
   private tasks = assignedTasks;
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -18,9 +26,16 @@ export class TaskService {
       userId,
       ...newTask,
     });
+
+    this.saveData();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveData();
+  }
+
+  private saveData() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
